c677654778bb65bedf7306fd984e452c5edf8270
Olatunde Oladeni <olatundeoladeni@yahoo.com>
2022-06-16 10:54:57 +0100

    xyz

---
+++ cypress.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: true,
+  defaultCommandTimeout: 8000,
+  pageLoadTimeout: 50000,
+  reporter: 'mochawesome',
+  projectId: 'kmom6q',
+  report: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quite: true,
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
